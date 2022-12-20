7a7fd3f479b086dc4cc3ce2e51556e846cdf01f2
BricePaquette <brice.paquette@gmail.com>
2022-12-20 08:34:00 -0600

    When requesting data from the ToDo table Category Info will not show as null

--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("OriginPolicy", "http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,5 +36,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
